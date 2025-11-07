b32c81dbd945b3d372d1eda04e0b94051e3d5b8f
luorijun <luorijun@outlook.com>
2025-11-07 16:24:23 +0800

    实现鼠标指向无极缩放

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,5 @@
 import '@/style.css'
-import { Application, Sprite, Texture } from 'pixi.js'
+import { Application, Container, Graphics, Sprite, Texture } from 'pixi.js'
 import { wrap } from 'comlink'
 import type { NoiseWorker } from './workers/noise'
 
@@ -16,17 +16,22 @@ el.appendChild(app.canvas)
 
 // start
 const chunk = { x: 0, y: 0 }
-const size = 64
-const maxLevel = 17
-const minOctaves = 2
+const size = 256
+const maxLevel = 13
+const minOctaves = 8
 
-const position = { x: 0, y: 0 }
-let zoom = 10
-let level = 0
+const center = { x: app.screen.width / 2, y: app.screen.height / 2 }
+const pointer = { x: 0, y: 0 }
+
+let level = -1
+let zoom = (app.screen.height - 40) / size
+const position = { x: center.x, y: center.y }
 
 // scene
-const sprite = new Sprite()
-app.stage.addChild(sprite)
+const world = new Container()
+app.stage.addChild(world)
+const sprite = new Sprite({ pivot: size / 2 })
+world.addChild(sprite)
 
 // noise
 const noise = wrap<NoiseWorker>(new Worker(new URL('./workers/noise.ts', import.meta.url), { type: 'module' }))
@@ -35,11 +40,16 @@ noise.set({ size: { width: size, height: size } })
 // zoom
 const zoomSpeed = 0.1
 const zoomMax = Math.pow(2, maxLevel)
-const zoomMin = 10
+const zoomMin = 1
 addEventListener('wheel', (e: WheelEvent) => {
   const dir = e.deltaY > 0 ? -1 : 1
-  zoom += zoom * zoomSpeed * dir
+  if ((dir < 0 && zoom <= zoomMin) || (dir > 0 && zoom >= zoomMax)) return
+  zoom *= (1 + zoomSpeed * dir)
   zoom = Math.max(zoomMin, Math.min(zoomMax, zoom))
+  position.x -= (pointer.x - world.x) * zoomSpeed * dir
+  position.y -= (pointer.y - world.y) * zoomSpeed * dir
+  dragStart = { x: e.clientX, y: e.clientY }
+  dragOffset = { x: position.x, y: position.y }
 })
 
 // drag
@@ -47,11 +57,14 @@ let drag = false
 let dragStart = { x: 0, y: 0 }
 let dragOffset = { x: 0, y: 0 }
 addEventListener('pointerdown', (e: PointerEvent) => {
+  if (!el.contains(e.target as Node)) return
   drag = true
   dragStart = { x: e.clientX, y: e.clientY }
   dragOffset = { x: position.x, y: position.y }
 })
 addEventListener('pointermove', (e: PointerEvent) => {
+  pointer.x = e.clientX - el.offsetLeft
+  pointer.y = e.clientY - el.offsetTop
   if (drag) {
     position.x = dragOffset.x + e.clientX - dragStart.x
     position.y = dragOffset.y + e.clientY - dragStart.y
@@ -63,10 +76,13 @@ addEventListener('pointerup', () => {
 
 // ticker
 app.ticker.add(() => {
-  debug.innerText = `FPS: ${Math.round(app.ticker.FPS)} zoom: ${zoom} level: ${level}`
+  const text = `zoom: ${zoom} level: ${level} octaves: ${minOctaves + level} length: ${Math.pow(2, level) * 256}`
+  if (debug.innerText != text) {
+    debug.innerText = text
+  }
 
-  app.stage.scale.set(zoom)
-  app.stage.position.set(position.x, position.y)
+  world.scale.set(zoom)
+  world.position.set(position.x, position.y)
 
   updateQTree()
 })
